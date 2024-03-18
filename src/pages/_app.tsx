f5f63646a78bf3fabae9fbbba5ae128f48fbf76f
Matt Carlotta <matt@mattcarlotta.com>
2024-03-18 13:17:30 -0700

    fix: adjust fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Rosarivo } from "next/font/google";
+import clsx from "clsx";
+
+const rosarivo = Rosarivo({ weight: "400", subsets: ["latin"], display: "swap" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <main className={clsx("flex min-h-screen flex-col items-center justify-center p-24", rosarivo.className)}>
+            <Component {...pageProps} />
+        </main>
+    )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
-import clsx from "clsx";
 import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
@@ -11,22 +8,20 @@ export default function Home() {
             <Head>
                 <title>Kady, Choose Your Adventure!</title>
             </Head>
-            <main className={clsx("flex min-h-screen flex-col items-center justify-center p-24", inter.className)}>
-                <section className="text-center flex flex-col justify-center items-center">
-                    <header>
-                        <h1 className="title text-5xl">CHOOSE YOUR ADVENTURE!</h1>
-                    </header>
-                    <p className="text-gray-300 text-md">
-                        Welcome, Kady! You have available adventures, choose one and start right away!
-                    </p>
-                    <Link
-                        className="glow w-44 mt-4 z-10 border border-black text-xl text-white px-4 py-2.5 cursor-pointer rounded hover:border-white"
-                        href="/choose-adventure/"
-                    >
-                        START
-                    </Link>
-                </section>
-            </main>
+            <section className="text-center flex flex-col justify-center items-center">
+                <header>
+                    <h1 className="title text-5xl font-bold">CHOOSE YOUR ADVENTURE!</h1>
+                </header>
+                <p className="text-md text-gray-300 tracking-wide">
+                    Welcome, Kady! You have 2nd date adventures available, please choose one to start!
+                </p>
+                <Link
+                    className="glow font-bold w-40 mt-8 z-10 border border-gray-400 text-xl text-gray-300 px-3 py-2 cursor-pointer rounded hover:border-white hover:text-white"
+                    href="/choose-adventure/"
+                >
+                    START
+                </Link>
+            </section>
         </>
     );
 }
