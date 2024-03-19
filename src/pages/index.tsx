e56cd88a8566be4b9dc55e5d419b09ddb10c19b2
Matt Carlotta <matt@mattcarlotta.com>
2024-03-18 18:23:45 -0700

    feat: add choose adventure page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,20 +8,22 @@ export default function Home() {
             <Head>
                 <title>Kady, Choose Your Adventure!</title>
             </Head>
-            <section className="text-center flex flex-col justify-center items-center">
-                <header>
-                    <h1 className="title text-5xl font-bold">CHOOSE YOUR ADVENTURE!</h1>
-                </header>
-                <p className="text-md text-gray-300 tracking-wide">
-                    Welcome, Kady! You have 2nd date adventures available, please choose one to start!
-                </p>
-                <Link
-                    className="glow font-bold w-40 mt-8 z-10 border border-gray-400 text-xl text-gray-300 px-3 py-2 cursor-pointer rounded hover:border-white hover:text-white"
-                    href="/choose-adventure/"
-                >
-                    START
-                </Link>
-            </section>
+            <div className="flex min-h-screen flex-col items-center justify-center p-24">
+                <section className="text-center flex flex-col justify-center items-center">
+                    <header>
+                        <h1 className="title text-5xl font-bold">CHOOSE YOUR ADVENTURE!</h1>
+                    </header>
+                    <p className="text-md text-gray-300 tracking-wide">
+                        Welcome, Kady! Press the START button below to view some available 2nd date adventures!
+                    </p>
+                    <Link
+                        className="glow font-bold w-40 mt-8 z-10 border border-gray-400 text-xl text-gray-300 px-3 py-2 cursor-pointer rounded hover:border-white hover:text-white"
+                        href="/choose-adventure/"
+                    >
+                        START
+                    </Link>
+                </section>
+            </div>
         </>
     );
 }
